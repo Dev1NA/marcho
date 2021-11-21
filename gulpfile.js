303bb8ba823ed350d7e9b9cc81ae1af6c8f2811a
Dev1NA <andreykoandroshchuk@gmail.com>
2021-11-21 21:40:41 +0200

    created popup section

--- gulpfile.js
+++ gulpfile.js
@@ -36,14 +36,15 @@ function styles() {
 
   function scripts() {
     return src([
-      'node_modules/jquery/dist/jquery.js',
-      'node_modules/slick-carousel/slick/slick.js',
-      'app/js/main.js'
-  ])  
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/slick-carousel/slick/slick.js",
+      "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
+      "app/js/main.js",
+    ])
+      .pipe(concat("main.min.js"))
+      .pipe(uglify())
+      .pipe(dest("app/js"))
+      .pipe(browserSync.stream());
 }
 
 function images() {
